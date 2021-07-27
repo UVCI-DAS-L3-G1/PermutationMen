fc6e6b606aecc8e52d9349ed9a8462113940cd87
Gnizako <86400730+Gnizako@users.noreply.github.com>
2021-07-27 20:11:54 +0000

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,10 +15,12 @@
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
+        @livewireScripts
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
+         @livewireScripts
     </body>
 </html>
